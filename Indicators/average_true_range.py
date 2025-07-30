b482e65aad1e5987897919d21c05c52fa58e47d0
VictorMinin <victor.a.minin@gmail.com>
2025-07-29 21:29:16 -0400

    Dropped unnecessary columns 'true range' 'tr1' etc

--- Indicators/average_true_range.py
+++ Indicators/average_true_range.py
@@ -39,5 +39,6 @@ class AverageTrueRange:
 
         # Calculate the Average True Range (ATR) using a simple moving average (SMA)
         df_atr[f'atr_{timeframe}'] = df_atr['true_range'].rolling(window=self.period).mean()
+        df_atr.drop(columns=['true_range', 'tr1', 'tr2', 'tr3'])
 
-        return df_atr[[f'atr_{timeframe}']]
+        return df_atr
